ba1ad3df130ce156ef79ae76ff79b2798f4e2adf
Veerman <Hasibuanfirman1404@gmail.com>
2026-02-23 22:11:27 +0700

    feat: add IN_CHANNELS nad OUT_CHANNELS config for U-Net and improve dataset path

--- configs/config.py
+++ configs/config.py
@@ -5,10 +5,15 @@ class Config:
     SEED = 42
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    DATA_ROOT = "/Dataset_BUSI_with_GT"
+    DATA = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', ))
+    DATA_ROOT = os.path.join(DATA, 'Dataset_BUSI_with_GT')
+
     BATCH_SIZE = 8
     NUM_WORKERS = 4
     IMAGE_SIZE = 256
+
+    IN_CHANNELS = 3
+    OUT_CHANNELS = 1
 
     EPOCHS = 30
     LR = 1e-4
